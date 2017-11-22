d34fb9c0611693357810de1ea5989740d2fce6ff
Rafael-Nogueira <30811920+Rafael-Nogueira@users.noreply.github.com>
2017-11-22 11:59:49 -0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,17 +1,52 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
+/* primesort
+ * rafael 147704
  */
 
 #include <stdio.h>
+#include <sys/time.h>
 
 int main() {
 
-  int x, y;
+	int x, n_divisores_x=0, j=0;
+	
+	FILE *loucura; //pipe louco criado
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+	loucura = popen("sort -n -r","w"); 
+
+	while (1){
+		inicio:
+		scanf("%d\n", &x);
+
+		if (x==-1) break;
+		
+		if ((x==0) || (x==1) || (x!=2 && x%2==0)){ // teste para excluir os números
+					       //0, 1 e pares (sem incluir o 2)	
+			fprintf(loucura, "%d\n", x);
+		}
+
+		else {   
+			n_divisores_x = 0;
+ 
+			for (j=2; j<=x; j++){	
+
+				if (x%j==0) //Dividi c por j[1, c]
+				{
+					n_divisores_x+=1;	
+				}
+				if (n_divisores_x>1){
+					
+					fprintf(loucura, "%d\n", x);
+
+					goto inicio;						
+			  }	
+
+		  }
+
+		}		
+	
+	}	
+
+	pclose(loucura); 
+	 
   return 0;
 }
